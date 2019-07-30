42c57d5bd43dcff8f7c3711dd003ab7eb7d8ce10
edwinwalela <edwinwalela@gmail.com>
2019-07-30 11:45:00 +0300

    product items

---
+++ src/components/search/Product.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Product(props) {
+    return (
+        <a href="">
+            <div style={productStyle}>
+                <img style={imageStyle} src="https://i.roamcdn.net/hz/pi/listing-thumb-543w/a3120aebf877204e7e4b71a737ee21d9/-/hzfiles/pi/picture/qd7x6v4/487dc6d83ddc348ef0123c17591b879c26069ee0.png"/>
+                <hr/>
+                <p style={nameStyle}>Product Name</p>
+                <h4 style={priceStyle}>KES 17,499</h4>
+                <img style={vendorBanner} src="/images/jumia.png"/>
+            </div>
+        </a>
+    )
+}
+
+const productStyle = {
+    display:"inline-block",
+    border:"solid 1px",
+    width:"90%",
+    maxWidth:"150px",
+    margin:"10px",
+    padding:"10px 20px",
+    textAlign:"left"
+}
+
+const imageStyle = {
+    width:"90%",
+    height:"100px",
+    margin:"10px auto",
+    display:"block",
+}
+
+const nameStyle = {
+    fontSize:"1em",
+    padding:"10px 0"
+}
+
+const priceStyle = {
+    fontSize:"0.9em",
+    color:"#333"
+}
+
+const vendorBanner = {
+    position:"relative",
+    right:"10px",
+    width:"80px",
+    height:"50px"
+}
+
+Product.propTypes = {
+
+}
+
+export default Product
+
--- src/components/search/ResultsContainer.js
+++ src/components/search/ResultsContainer.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Product from './Product';
+
 function ResultsContainer(props) {
     return (
-        <div>
-            
+        <div style={containerStyle}>
+        <p style={metaStyle}>Showing 10 results from 3 vendors</p>
+            <Product />
+            <Product />
+            <Product />
+            <Product />
         </div>
     )
+}
+
+const metaStyle = {
+    fontSize:"0.8em",
+    color:"#333",
+    padding:"10px"
+}
+
+const containerStyle = {
+    width:"100%",
+    margin:"30px auto 50px auto",
+    textAlign:"center"
 }
 
 ResultsContainer.propTypes = {
